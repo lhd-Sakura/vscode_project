adb1dc3344519a96cbf5119166089f647aa2d759
lhd-Sakura <1285446775@qq.com>
2020-10-22 19:53:10 +0800

    2020.10.22

--- learning_c++/Initialization_list.cpp
+++ learning_c++/Initialization_list.cpp
@@ -1,5 +1,6 @@
 /*
     初始化列表
+    C++提供了初始化列表语法，用来初始化属性
 
 */
 
@@ -7,16 +8,30 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+class Person
 {
-    
-    return 0;
+public:
+    Person() : m_A(10), m_B(20), m_C(30)
+    {
+    }
+
+    int m_A;
+    int m_B;
+    int m_C;
+
+};
+
+void test01()
+{
+    Person p;
+    cout << "m_A = " << p.m_A << endl;
+    cout << "m_B = " << p.m_B << endl;
+    cout << "m_C = " << p.m_C << endl;
 }
 
+int main(int argc, char const *argv[])
+{
+    test01();
 
-
-
-
-
-
-
+    return 0;
+}
